e357a94dfa9ce1c59195a65c9d22939f875a61e7
Deemon13 <51171522+Deemon13@users.noreply.github.com>
2020-06-10 22:29:48 +0300

    mainPage v1 with merging dev

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheet/main.css';
-import App from './components/App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./stylesheet/main.css";
 import App from "./components/App";
->>>>>>> dev
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,9 +11,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-<<<<<<< HEAD
-  document.getElementById('root')
-=======
   document.getElementById("root")
->>>>>>> dev
 );
