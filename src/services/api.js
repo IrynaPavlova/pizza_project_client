53302fc55e108002033466c39686cea5714bb763
Alex Bakhteyev <a.bakhteyev@gmail.com>
2020-06-10 19:40:01 -0400

    PizzaListItem commponent

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseURL = 'https://evening-caverns-34846.herokuapp.com/';
-
-const fetchPizzaDetailsId = pizzaId  => {
-    return fetch(`${baseURL}/products/${pizzaId}`)
-    .then(res => res.json());
-};
-
-export default fetchPizzaDetailsId;
