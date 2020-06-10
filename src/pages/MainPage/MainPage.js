7d4843473d8657737cccbe18cc274c1c4b32c32f
Alex Bakhteyev <a.bakhteyev@gmail.com>
2020-06-10 19:52:46 -0400

    PizzaListItem commponent

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React from "react";
 
 // import { Switch, Route } from 'react-router-dom';
 // import { routes } from '../../servises/routes';
@@ -9,7 +8,6 @@ const MainPage = () => {
   return (
     <main>
       <h2>MainPage</h2>
-      
     </main>
   );
 };
