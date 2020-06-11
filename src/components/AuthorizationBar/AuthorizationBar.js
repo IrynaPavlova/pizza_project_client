81d3cb5e3cdb533644b8d6308087de5f2b3c9d43
Pavlova <irochkapavlova85@gmail.com>
2020-06-11 15:48:49 +0300

    edit

--- src/components/AuthorizationBar/AuthorizationBar.js
+++ src/components/AuthorizationBar/AuthorizationBar.js
@@ -3,11 +3,12 @@ import React from "react";
 import styles from "./AuthorizationBar.module.css";
 
 export default function AuthorizationBar() {
+  let isAuthenticated = true;
   return (
     <>
       {isAuthenticated ? (
         <>
-          <p className={styles.name}>{name}</p>
+          <p className={styles.name}>name</p>
           <button type="button" className={styles.logoutButton}>
             Выйти
           </button>
