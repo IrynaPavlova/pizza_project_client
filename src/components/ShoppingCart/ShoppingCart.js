a7bb8824ba90c4b7409b7b5394568ec12e1b021e
Pavlova <irochkapavlova85@gmail.com>
2020-06-11 12:31:02 +0300

    edits

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -6,7 +6,7 @@ import styles from "./shoppingCart.module.css";
 const ShoppingCart = ({ amount = 0, price, onOpenOrderPage }) => {
   if (amount === 0) {
     return (
-      <div >
+      <div>
         <div className={styles.productsAmount}>
           <span className={styles.productsAmountText}>{amount}</span>
         </div>
