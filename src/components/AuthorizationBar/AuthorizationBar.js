0a82bc40b42e251273d0f8d756aa542dae3b7106
EmilioRosso <7zen@ukr.net>
2020-06-11 16:55:54 +0300

    navigation fix

--- src/components/AuthorizationBar/AuthorizationBar.js
+++ src/components/AuthorizationBar/AuthorizationBar.js
@@ -5,19 +5,19 @@ import styles from "./AuthorizationBar.module.css";
 export default function AuthorizationBar() {
   let isAuthenticated = true;
   return (
-    <>
+    <div>
       {isAuthenticated ? (
-        <>
+        <div className={styles.userWrapper}>
           <p className={styles.name}>name</p>
           <button type="button" className={styles.logoutButton}>
             Выйти
           </button>
-        </>
+        </div>
       ) : (
         <button type="button" className={styles.loginButton}>
           Войти
         </button>
       )}
-    </>
+    </div>
   );
 }
