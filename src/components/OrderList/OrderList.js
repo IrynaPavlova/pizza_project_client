2f99b7b426a632e9a28f4092a49748c78ee66585
marcusfenix1 <48603513+marcusfenix1@users.noreply.github.com>
2020-06-13 19:57:42 +0300

    init OrderList

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -1,16 +1,16 @@
 import React from "react";
 import styles from "./OrderList.module.css";
-import OrderListItem from "./OrderListItem/OrderListItem";
+import OrderListItem from "../OrderListItem/OrderListItem";
 
 const orderList = (_) => {
   return (
     <div className={styles.orderList}>
       <h2 className={styles.orderListTitle}>Ваш заказ</h2>
       <ul>
-        {/* <OrderListItem />
-      <OrderListItem />
-      <OrderListItem />
-      <OrderListItem /> */}
+        <OrderListItem />
+        <OrderListItem />
+        <OrderListItem />
+        <OrderListItem />
       </ul>
       <p className={styles.orderListPrice}>
         {"298"}
