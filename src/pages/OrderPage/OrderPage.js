f3ff89d7a2ccb2cefdcee1d369a00c7510ef8e7f
EmilioRosso <7zen@ukr.net>
2020-06-13 14:38:10 +0300

    n finished

--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -2,14 +2,13 @@ import React from "react";
 import OrderList from "../../components/OrderList";
 import styles from "./OrderPage.module.css";
 //import OrderForm from "../../components/OrderForm";
-import AdminOrderList from "../../components/AdminOrderList/AdminOrderList.js";
 
 const OrderPage = () => {
   return (
     <div className={styles.OrderPageContainer}>
       <hr className={styles.line} />
       <OrderList />
-      <AdminOrderList />
+
       <hr className={styles.line} />
 
       {/* <OrderForm /> */}
