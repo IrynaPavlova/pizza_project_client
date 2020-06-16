c70b41ec10aab3ab662e8ecaeecf621cf841e28a
marcusfenix1 <48603513+marcusfenix1@users.noreply.github.com>
2020-06-16 16:27:45 +0300

    init OrdersList

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "./OrderList.module.css";
 import OrderListItem from "../OrderListItem/OrderListItem";
+import orderItems from "../../services/orderItems.json";
 
-const orderList = (_) => {
+const orderList = () => {
   return (
     <div className={styles.orderList}>
       <h2 className={styles.orderListTitle}>Ваш заказ</h2>
       <ul>
-        <OrderListItem />
-        <OrderListItem />
-        <OrderListItem />
-        <OrderListItem />
+        {orderItems.map(({ _id, images, name, price, ingredients }) => (
+          <OrderListItem
+            key={_id}
+            img={images[0]}
+            name={name}
+            price={price}
+            ingredients={ingredients}
+          />
+        ))}
       </ul>
       <p className={styles.orderListPrice}>
         {"298"}
