53f9a5e6da7adfa050ced82301041877e273cafc
Pavlova <irochkapavlova85@gmail.com>
2020-06-16 22:23:06 +0300

    edits

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ import PizzaList from "../components/PizzaList/PizzaListContainer";
 
 import Header from "./Header";
 import Spinner from "./Spinner";
-//import Footer from "./Footer/Footer";
+import Footer from "./Footer/Footer";
 
 const DessertsList = lazy(() => import("../components/DessertsList"));
 
@@ -41,7 +41,7 @@ const App = () => {
           {/* <Redirect to="#" /> */}
         </Switch>
       </Suspense>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 };
