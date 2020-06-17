86e2668ea7382d465df530c2ffe03e5493db64aa
EmilioRosso <7zen@ukr.net>
2020-06-17 14:11:16 +0300

    intl

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { IntlProvider } from "react-intl";
+
 import { authOperations } from "../redux/auth";
 import { routes } from "../services/routes";
 import MainPage from "../pages/MainPage";
@@ -18,31 +20,48 @@ import Header from "./Header";
 import Spinner from "./Spinner";
 import Footer from "./Footer/Footer";
 
+import localMessages from "../languages";
+
 const DessertsList = lazy(() => import("../components/DessertsList"));
 
 const App = () => {
   const dispatch = useDispatch();
+
+  const local = useSelector((state) => state.local);
+
   useEffect(() => {
     dispatch(authOperations.getCurrentUser());
   }, [dispatch]);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Suspense fallback={<Spinner />}>
-        <Switch>
-          <Route path={routes.MAIN_PAGE} exact component={MainPage} />
-          <Route path={routes.DESSERTS} exact component={DessertsList} />
-          <Route path={routes.PIZZA} component={PizzaList} />
-          <Route path={routes.PROMO} component={PromoList} />
-          <Route path={routes.ORDER_PAGE} component={OrderPage} />
-          <Route path={routes.ADMIN_PAGE} component={AdminPage} />
-          <Route path={routes.ABOUT_DEV_PAGE} component={AboutDevelopersPage} />
-          <Route path={routes.AUTH} component={AuthPage} />
-          {/* <Redirect to="#" /> */}
-        </Switch>
-      </Suspense>
-      <Footer />
-    </BrowserRouter>
+    <>
+      <IntlProvider
+        locale={local}
+        defaultLocale="ru"
+        messages={localMessages[local]}
+      >
+        <BrowserRouter>
+          <Header />
+          <Suspense fallback={<Spinner />}>
+            <Switch>
+              <Route path={routes.MAIN_PAGE} exact component={MainPage} />
+              <Route path={routes.DESSERTS} exact component={DessertsList} />
+              <Route path={routes.PIZZA} component={PizzaList} />
+              <Route path={routes.PROMO} component={PromoList} />
+              <Route path={routes.ORDER_PAGE} component={OrderPage} />
+              <Route path={routes.ADMIN_PAGE} component={AdminPage} />
+              <Route
+                path={routes.ABOUT_DEV_PAGE}
+                component={AboutDevelopersPage}
+              />
+              <Route path={routes.AUTH} component={AuthPage} />
+              {/* <Redirect to="#" /> */}
+            </Switch>
+          </Suspense>
+          <Footer />
+        </BrowserRouter>
+      </IntlProvider>
+    </>
   );
 };
 
