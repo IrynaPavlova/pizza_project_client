b0e103de3ab5d8b87364d948d3866aff5f75c5ea
Deemon13 <51171522+Deemon13@users.noreply.github.com>
2020-06-24 15:23:32 +0300

    slider - promoLink

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Carousel } from "react-bootstrap";
+
+import { routes } from "../../services/routes";
 
 import styles from "./Slider.module.css";
 
@@ -16,21 +19,23 @@ export default function Slideview(props) {
             title={title[local]}
             description={description[local]}
           >
-            <div className={styles.sliderWrapper}>
-              <img
-                className={styles.sliderImg + " d-block w-100 h-100"}
-                src={images}
-                alt=""
-              />
-              <div className={styles.sliderOverlay}>
-                <div className={styles.sliderContent}>
-                  <p className={styles.sliderPromoDescription}>
-                    {description[local]}
-                  </p>
-                  <h2 className={styles.slidePromoName}>{title[local]}</h2>
+            <NavLink to={routes.PROMO}>
+              <div className={styles.sliderWrapper}>
+                <img
+                  className={styles.sliderImg + " d-block w-100 h-100"}
+                  src={images}
+                  alt=""
+                />
+                <div className={styles.sliderOverlay}>
+                  <div className={styles.sliderContent}>
+                    <p className={styles.sliderPromoDescription}>
+                      {description[local]}
+                    </p>
+                    <h2 className={styles.slidePromoName}>{title[local]}</h2>
+                  </div>
                 </div>
               </div>
-            </div>
+            </NavLink>
           </Carousel.Item>
         );
       })}
