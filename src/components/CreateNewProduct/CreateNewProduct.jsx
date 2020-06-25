b0b95eb87fa4483556088edf73bc59317a0d08fb
Valeri <valeria.chyornaya@gmail.com>
2020-06-25 06:27:42 +0300

    add redux to CreateNewProd

--- src/components/CreateNewProduct/CreateNewProduct.jsx
+++ src/components/CreateNewProduct/CreateNewProduct.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "./CreateNewProduct.module.css";
 import Select from "react-select";
-import {
-  getAllIngredients,
-  postNewProduct,
-  postImage,
-} from "../../services/api";
+import productSelectors from "../../redux/product/productSelectors";
+import productOperations from "../../redux/product/productOperations";
+import Notification from "../Notification";
 
 const categories = {
   pizza: "pizza",
@@ -31,8 +30,6 @@ const pizzaCategories = [
 const CreateNewProduct = () => {
   const [category, changeCategory] = useState(options[0]);
   const [subCategory, changeSubCategory] = useState(pizzaCategories[0]);
-  // const handle
-  const [ingredients, setIngredients] = useState([]);
   const [ukrName, setUkrName] = useState("");
   const [enName, setEnName] = useState("");
   const [ruName, setRuName] = useState("");
@@ -41,13 +38,27 @@ const CreateNewProduct = () => {
   const [L, setL] = useState("");
   const [XL, setXL] = useState("");
   const [description, setDescription] = useState("");
-  useEffect(() => {
-    getAllIngredients().then((data) => {
-      if (JSON.stringify(data) !== JSON.stringify(ingredients))
-        setIngredients(data);
-    });
-  });
   const [activeIngredients, setActiveIngredients] = useState([]);
+
+  const ingredients = useSelector(productSelectors.getIngradients);
+  const dispatch = useDispatch();
+  const postImage = (file) => dispatch(productOperations.sendFile(file));
+  const hrefProductImd = useSelector(productSelectors.fileLink);
+  const postNewProduct = (product) =>
+    dispatch(productOperations.sendProduct(product));
+  const createdProduct = useSelector(productSelectors.getProducts);
+  //   console.log("hrefProductImd:", hrefProductImd);
+
+  useEffect(() => {
+    dispatch(productOperations.getIngredients());
+  }, [dispatch]);
+
+  const handleImg = (e) => {
+    e.persist();
+    e.preventDefault();
+    postImage(e.target.files[0]);
+  };
+
   const handleCheckboxChange = (e) => {
     e.persist();
     if (e.target.checked) {
@@ -59,12 +70,10 @@ const CreateNewProduct = () => {
   const submitForm = async (e) => {
     e.persist();
     e.preventDefault();
-    let res = await postImage(e.target.querySelector("#image").files[0]);
-    const href = res.data.image.file;
     const product = {
       categories: category.value,
       currency: "грн",
-      images: href,
+      images: hrefProductImd,
       name: {
         ukr: ukrName,
         en: enName,
@@ -85,11 +94,14 @@ const CreateNewProduct = () => {
     } else {
       product.price = { price };
     }
-    res = await postNewProduct(product);
-    alert(res.data.status);
+    postNewProduct(product);
   };
+
   return (
     <div className={styles.createContainer}>
+      {createdProduct.length === 1 && (
+        <Notification message={`Создан продукт`} confirm forCard />
+      )}
       <form onSubmit={submitForm} className={styles.applyForm}>
         <Select
           className={styles.select}
@@ -179,7 +191,7 @@ const CreateNewProduct = () => {
 
         <label className={styles.inputLabel}>
           <h4>Image</h4>
-          <input type="file" id="image" />
+          <input type="file" id="image" onChange={handleImg} />
         </label>
         <div className={styles.ingredientsContainer}>
           {category.value === categories.pizza &&
