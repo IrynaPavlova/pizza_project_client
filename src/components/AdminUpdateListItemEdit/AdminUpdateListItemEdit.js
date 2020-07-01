c0e71905825ed30691b29018ca3c3c78b154f94d
ViktorSpivak <49995904+ViktorSpivak@users.noreply.github.com>
2020-07-01 10:48:20 +0300

    fix delete

--- src/components/AdminUpdateListItemEdit/AdminUpdateListItemEdit.js
+++ src/components/AdminUpdateListItemEdit/AdminUpdateListItemEdit.js
@@ -219,7 +219,6 @@ const AdminUpdateListItemEdit = () => {
           )}
         </form>
         <button
-          disabled={!isLoading}
           form="editForm"
           type="submit"
           name="complete"
@@ -228,7 +227,6 @@ const AdminUpdateListItemEdit = () => {
           <FormattedMessage id="update.saveChanges" />
         </button>
         <button
-          disabled={!isLoading}
           form="editForm"
           type="submit"
           name="delete"
