654ea3e21c8693773146f44c96912cf7e9100410
Pavlova <irochkapavlova85@gmail.com>
2020-07-07 17:18:29 +0300

    edits

--- src/components/CreateNewProduct/CreateNewProduct.jsx
+++ src/components/CreateNewProduct/CreateNewProduct.jsx
@@ -166,6 +166,7 @@ const CreateNewProduct = () => {
             <p className={styles.title}>
               <FormattedMessage id="product.subcategory" />
             </p>
+            {/* {console.log("category", category)} */}
             <Select
               className={styles.select}
               value={category.value === categories.pizza ? subCategory : null}
